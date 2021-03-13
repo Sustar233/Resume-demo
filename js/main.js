3a155efedd4510a6844a9a55dff0af15cd9b531c
sustar <3sustar@gmail.com>
2021-03-13 19:28:54 +0800

    页面移动到相应位置会激活相应顶部导航项

--- js/main.js
+++ js/main.js
@@ -8,16 +8,45 @@ window.onscroll = function(e) {
     }
   }
 
-// 鼠标移入顶部导航项时给li添加active样式
+  // 页面到相应区域时对应顶部导航项高亮
 let topNav = document.getElementById('topNav');
-let topNavlis = topNav.querySelectorAll('#topNav>ul>li')
-for (let menuli of topNavlis) {
-    menuli.addEventListener("mouseover", function(e) {
-        menuli.classList.add('active');
-    });
-    menuli.addEventListener("mouseout", function(e) {
-        menuli.classList.remove('active')
-    });
+let dataTops = document.querySelectorAll("[data-top]");
+let topNavlis = topNav.querySelectorAll('#topNav>ul>li');
+let act = 0;
+topNavlis[act].classList.add('active');
+let checkTop = function() {
+  for (let i = 0; i < dataTops.length; i++) {
+    // 如果 滚动位置 - 该项距离顶部位置 < 滚动位置 - 被标记项离顶部位置，则修改标记项（修改成距离最近的
+    if ( Math.abs(window.scrollY - dataTops[i].offsetTop) < Math.abs(window.scrollY - dataTops[act].offsetTop)) {
+      topNavlis[act].classList.remove('active');
+      act = i;
+    }
+    topNavlis[act].classList.add('active');
+  } 
+}
+window.addEventListener("scroll", checkTop);
+
+// 鼠标移入顶部导航项时给li添加active和showSubmenu样式
+for (let i = 0; i < topNavlis.length; i++) {
+  topNavlis[i].addEventListener("mouseover", function(e) {
+    // 先移除所有其他li的active
+    for (let i = 0; i < topNavlis.length; i++) {
+      topNavlis[i].classList.remove('active');
+    }
+    // 再为当前鼠标选中元素添加active
+    topNavlis[i].classList.add('active');
+    topNavlis[i].classList.add('showSubmenu');
+  });
+  topNavlis[i].addEventListener("mouseout", function(e) {
+    topNavlis[i].classList.remove('showSubmenu');
+    // 当鼠标离开的不是高亮的li时
+    if ( i !== act ) {
+      // 先将应有active（对应位置）的li添加回active
+      checkTop()
+      // 再将次li的active移除
+      topNavlis[i].classList.remove('active');
+    }
+  });
 }
 
 // 鼠标点击顶部导航项时页面滑动到相应位置
@@ -31,7 +60,21 @@ for (let a of topNavA) {
         let ele = document.querySelector(href);
         // 获取元素距离页面顶端的距离
         let top = ele.offsetTop;
-        // 视窗移动到元素位置
-        window.scrollTo(0, top - 80);
+
+        let n = 30 // 一共动多少次
+        let duration = 500 / n // 多少时间动一次
+        let currentTop = window.scrollY; // 当前滚动位置
+        let targetTop = top - 80;  // 目标位置
+        let distance = (targetTop - currentTop) / n; //单次移动距离/一段
+        let i = 0;
+        let id = setInterval( ()=> {
+          if (i===n) {
+            window.clearInterval(id);
+            return
+          }
+          i++;
+          window.scrollTo(0, currentTop + distance * i);
+        }, duration)
     }
 }
+
