08dcaa691ca4cd905350e22cf1519eb56869c212
sustar <3sustar@gmail.com>
2021-12-18 23:47:10 +0800

    portfolio1.onclick = function() {

---
+++ js/portfolio.js
@@ -0,0 +1,11 @@
+portfolio1.onclick = function() {
+  portfolioBar.className = 'bar state-1'
+}
+
+portfolio2.onclick = function() {
+  portfolioBar.className = 'bar state-2'
+}
+
+portfolio3.onclick = function() {
+  portfolioBar.className = 'bar state-3'
+}
